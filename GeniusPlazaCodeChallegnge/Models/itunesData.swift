4d31029f5363843b67293750974dfbc1396be513
Kadeem Palacios <kadeempalacios@gmail.com>
2019-04-11 16:07:00 -0700

    project creation

---
+++ GeniusPlazaCodeChallegnge/Models/itunesData.swift
@@ -0,0 +1,12 @@
+//
+//  itunesData.swift
+//  GeniusPlazaCodeChallegnge
+//
+//  Created by Kadeem Palacios on 4/11/19.
+//  Copyright © 2019 Kadeem Palacios. All rights reserved.
+//
+
+import Foundation
+struct itunesData {
+    
+}
