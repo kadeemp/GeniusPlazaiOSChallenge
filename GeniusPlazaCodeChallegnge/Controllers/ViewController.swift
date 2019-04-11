18c1b56213a18e31bab1d6e9cbaaa5a68068729a
Kadeem Palacios <kadeempalacios@gmail.com>
2019-04-11 16:17:53 -0700

    basic tableview implemented

--- GeniusPlazaCodeChallegnge/Controllers/ViewController.swift
+++ GeniusPlazaCodeChallegnge/Controllers/ViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var itunesTableView:UITableView!
+    var itunesInfo:[itunesData] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupTableView()
+
     }
+
+    func setupTableView() {
+        itunesTableView = UITableView()
+        itunesTableView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width - 40, height: self.view.frame.height)
+        itunesTableView.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY)
+        itunesTableView.delegate = self
+        itunesTableView.dataSource = self
+        itunesTableView.register(itunesTableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.view.addSubview(itunesTableView)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = itunesTableView.dequeueReusableCell(withIdentifier: "Cell") as! itunesTableViewCell
+        cell.textLabel?.text = "Test"
+        cell.backgroundColor = UIColor.lightGray
+        return cell
+    }
+
+
+
 
 
 }
